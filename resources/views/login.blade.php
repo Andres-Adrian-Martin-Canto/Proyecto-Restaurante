6ccfe9f019af13edd3b42de1a7c59328a0120362
Cdark21 <cdark2105@gmail.com>
2025-05-07 17:40:56 -0500

    Agregación de login y Index

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Titulo--------------------------------------------------------------------------------------------------------------------->
+    <title>Login Gokai</title>
+    <!--Css------------------------------------------------------------------------------------------------------------------------>
+    @vite(['resources/css/principal.css', 'resources/js/principal.js'])
+    <!--Favicon-------------------------------------------------------------------------------------------------------------------->
+    <link rel="icon" href="{{ asset('Favicon.png') }}" type="image/x-icon">
+</head>
+<body>
+    <h1>Login</h1>
+</body>
+</html>
