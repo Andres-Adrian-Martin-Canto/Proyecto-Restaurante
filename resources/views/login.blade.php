16c339aeb86166ee5302c330320e8467d281c7d9
Cdark21 <cdark2105@gmail.com>
2025-05-12 22:50:26 -0500

    Login y rutas para el login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--Titulo--------------------------------------------------------------------------------------------------------------------->
     <title>Login Gokai</title>
     <!--Css------------------------------------------------------------------------------------------------------------------------>
-    @vite(['resources/css/principal.css', 'resources/js/principal.js'])
+    @vite(['resources/css/login.css', 'resources/js/login.js'])
     <!--Favicon-------------------------------------------------------------------------------------------------------------------->
     <link rel="icon" href="{{ asset('Favicon.png') }}" type="image/x-icon">
 </head>
+
 <body>
-    <h1>Login</h1>
+    <h1 class="titulo">GOKAI ASIAN FOOD</h1>
+    <div class="login-wrapper">
+        <div class="login-card">
+            <div class="login-title">
+                <h2>LOGIN</h2>
+            </div>
+            <form>
+                <label for="email">Correo electrónico<span>*</span></label>
+                <input type="email" id="email" placeholder="administrador@ejemplo.com" required />
+
+                <label for="password">Contraseña<span>*</span></label>
+                <input type="password" id="password" placeholder="***************" required />
+
+                <button type="submit">Iniciar sesión</button>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
